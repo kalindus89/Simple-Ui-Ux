0fdd86700afa8dd39819a461eb0d3122f79d57fc
kalin <kalindusupun@gmail.com>
2021-10-10 10:55:06 +0530

    Simple-Ui-Ux

--- app/src/main/java/com/simple_ui_ux/full_ui/FullUi_Activity_1.java
+++ app/src/main/java/com/simple_ui_ux/full_ui/FullUi_Activity_1.java
@@ -2,9 +2,14 @@ package com.simple_ui_ux.full_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.simple_ui_ux.R;
+import com.simple_ui_ux.profile.Profile_Activity_1;
+import com.simple_ui_ux.profile.Profile_MainActivity;
 
 public class FullUi_Activity_1 extends AppCompatActivity {
 
@@ -12,5 +17,44 @@ public class FullUi_Activity_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_ui1);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.intro_signUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.uiSignup).setVisibility(View.VISIBLE);
+                findViewById(R.id.uiIntro).setVisibility(View.GONE);
+                findViewById(R.id.uiSignIn).setVisibility(View.GONE);
+                findViewById(R.id.uiMenu).setVisibility(View.GONE);
+            }
+        });
+        findViewById(R.id.intro_signIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.uiSignup).setVisibility(View.VISIBLE);
+                findViewById(R.id.uiIntro).setVisibility(View.GONE);
+                findViewById(R.id.uiSignIn).setVisibility(View.GONE);
+                findViewById(R.id.uiMenu).setVisibility(View.GONE);
+            }
+        });
+        findViewById(R.id.signUpMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.uiSignup).setVisibility(View.GONE);
+                findViewById(R.id.uiIntro).setVisibility(View.GONE);
+                findViewById(R.id.uiSignIn).setVisibility(View.VISIBLE);
+                findViewById(R.id.uiMenu).setVisibility(View.GONE);
+            }
+        });
+        findViewById(R.id.signInMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.uiSignup).setVisibility(View.GONE);
+                findViewById(R.id.uiIntro).setVisibility(View.GONE);
+                findViewById(R.id.uiSignIn).setVisibility(View.GONE);
+                findViewById(R.id.uiMenu).setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
--- app/src/main/java/com/simple_ui_ux/full_ui/FullUi_MainActivity.java
+++ app/src/main/java/com/simple_ui_ux/full_ui/FullUi_MainActivity.java
@@ -2,10 +2,14 @@ package com.simple_ui_ux.full_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.simple_ui_ux.R;
+import com.simple_ui_ux.profile.Profile_Activity_1;
+import com.simple_ui_ux.profile.Profile_MainActivity;
 
 public class FullUi_MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,13 @@ public class FullUi_MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_full_ui_main);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.menu1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FullUi_MainActivity.this, FullUi_Activity_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
